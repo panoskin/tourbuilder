7dede4c8bfd1f1468a6a47a8c59739d289d054a8
Kewin Polok <kpolok@lcpmedia.com>
2023-04-28 11:20:37 +0200

    Deploy .dev script versions for develop branch

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,6 @@ const terser = require("@rollup/plugin-terser");
 module.exports = {
   input: "tourbuilder.js",
   output: {
-    file: "dist/tourbuilder.min.js",
     format: "iife",
   },
   plugins: [terser()],
